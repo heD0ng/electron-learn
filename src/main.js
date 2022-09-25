f75588d2290b2efb700eac1e469a33fc8777f16c
hedong <13545403924@163.com>
2022-09-25 14:02:07 +0800

    first-finish

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Directive from '@/directives'
+import '@/assets/styles/reset.scss'
+import '@/assets/styles/comm.scss'
+
+const app = createApp(App)
+app.use(Directive)
+app.use(router)
+
+app.mount('#app')
